ba0ba310d8dfeb067ad1790b04a9d26c0b852e96
DrLiamKless <67854560+DrLiamKless@users.noreply.github.com>
2021-04-08 12:21:26 +0300

    change server architecture

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,7 @@
-import express from 'express';
-import path from 'path';
+
+import app from './app';
 
 const port = process.env.PORT || 8080;
-
-const app: express.Application = express();
-
-app.use('/', express.static('../build/'));
-
-app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
-});
 
 app.listen(port, () => {
   console.log(`Elevators is listening on port ${port}!`);
